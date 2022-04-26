23d8e5d53db79deff84106b7083248f655177046
cativo45 <ccativo@mibluemedical.com>
2022-04-26 02:04:16 -0600

    🔨 Add get user info

---
+++ src/user/user.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+
+@UseGuards(JwtGuard)
+@Controller({
+  path: 'users',
+  version: '1',
+})
+export class UserController {
+  @HttpCode(HttpStatus.OK)
+  @Get('me')
+  me(@GetUser() user: User) {
+    return user;
+  }
+}
