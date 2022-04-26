a2b3088d34ba554f6dc7c9901dd8795ae57874d6
cativo45 <ccativo@mibluemedical.com>
2022-04-26 02:10:15 -0600

    🔨 Set app.module correct

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { UserModule } from './user/user.module';
 import { HomeController } from './home/home.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { DonationModule } from './donation/donation.module';
+import { ContactModule } from './contact/contact.module';
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule, ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DonationModule,
     ContactModule,
+  ],
   controllers: [HomeController],
 })
 export class AppModule {}
