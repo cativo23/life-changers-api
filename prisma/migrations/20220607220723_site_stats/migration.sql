c494c05ad8a6a4f0deedc2b44762927a98fc5ca9
cativo23 <cativo23.kt@gmail.com>
2022-06-08 01:03:23 -0600

    Add site-stats

---
+++ prisma/migrations/20220607220723_site_stats/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `site_stats` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `total` INTEGER NOT NULL DEFAULT 0,
+    `description` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
