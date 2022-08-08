bcb5cde2308f177fb902c3f9e7febb8cdf82a1ba
cativo23 <cativo23.kt@gmail.com>
2022-08-07 23:36:47 -0600

    🔨 Change root endpoint

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('/')
 export class HomeController {
+  constructor(private config: ConfigService) {}
+
   @Get('/')
   login() {
     return {
-      message: 'Welcome to the API',
+      message: 'Welcome to ' + this.config.get('APP_NAME'),
       data:
       {
         'life-changers-api': 'v1.0.0',
